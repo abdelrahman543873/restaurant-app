c29fc12bdb6d9d83cdf90e995395a9713e74edc3
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-12-21 13:45:35 +0200

    feat: added switching to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() selection = new EventEmitter<string>();
+
+  onRecipeClick() {
+    this.selection.emit("recipes");
+  }
+
+  onShoppingClick() {
+    this.selection.emit("shopping");
+  }
+}
