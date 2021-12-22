6ab8a8b2b4891daa3f7fedbe535cc950867c2895
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-12-22 11:16:32 +0200

    feat: added ingredient adding feature

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"],
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  addIngredients(
+    event: PointerEvent,
+    amountInput: HTMLInputElement,
+    nameInput: HTMLInputElement
+  ) {
+    this.ingredientAdded.emit({
+      name: nameInput.value,
+      amount: +amountInput.value,
+    });
   }
-
 }
