fa998f556b0832b2c3357d35184561e470d51897
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-12-27 15:10:18 +0200

    feat: manage ingredient list feat

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { ShoppingListService } from "../../shopping-list/shopping-list.service";
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
   selector: "app-recipe-detail",
@@ -7,8 +9,14 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-detail.component.css"],
 })
 export class RecipeDetailComponent implements OnInit {
-  constructor() {}
+  constructor(private shoppingListService: ShoppingListService) {}
   @Input() recipe: Recipe;
 
   ngOnInit() {}
+
+  addIngredients(ingredients: Ingredient[]) {
+    ingredients.forEach((ingredient) => {
+      this.shoppingListService.addIngredient(ingredient);
+    });
+  }
 }
