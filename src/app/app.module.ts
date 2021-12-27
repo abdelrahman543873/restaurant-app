08ff6a17ffd98a3b8acb31da7662c2cd46399604
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-12-27 20:47:07 +0200

    feat: added recipe link active naviagation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
+import { RoutingModule } from "./routing.module";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,7 @@ import { DropdownDirective } from "./shared/dropdown.directive";
     ShoppingEditComponent,
     DropdownDirective,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,14 +4,4 @@ import { Component, EventEmitter, Output } from "@angular/core";
   selector: "app-header",
   templateUrl: "./header.component.html",
 })
-export class HeaderComponent {
-  @Output() selection = new EventEmitter<string>();
-
-  onRecipeClick() {
-    this.selection.emit("recipes");
-  }
-
-  onShoppingClick() {
-    this.selection.emit("shopping");
-  }
-}
+export class HeaderComponent {}
