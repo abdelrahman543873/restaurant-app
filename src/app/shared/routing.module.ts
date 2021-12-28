1916570a01598509d6be56a8ba8a2ed0de4e67c5
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-12-28 10:04:40 +0200

    refactoring routing module

--- src/app/shared/routing.module.ts
+++ src/app/shared/routing.module.ts
@@ -1,9 +1,9 @@
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeDetailComponent } from "../recipes/recipe-detail/recipe-detail.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipesComponent } from "../recipes/recipes.component";
+import { RecipeStartComponent } from "../recipes/recipe-start/recipe-start.component";
+import { ShoppingListComponent } from "../shopping-list/shopping-list.component";
 
 const routes: Routes = [
   { path: " ", redirectTo: "recipes", pathMatch: "full" },
