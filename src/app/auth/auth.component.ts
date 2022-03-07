7dd7b8b123f0fc1bc154b111915f3cf908db26f1
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2022-03-07 14:47:30 +0200

    feat: added error handling logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Component({
@@ -10,6 +11,7 @@ import { AuthService } from "./auth.service";
 export class AuthComponent implements OnInit {
   isLoginMode = true;
   isLoading = false;
+  error: string = null;
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
@@ -18,16 +20,25 @@ export class AuthComponent implements OnInit {
     this.isLoginMode = !this.isLoginMode;
   }
   onSubmit(input: NgForm) {
-    if (!input.form.valid) return;
     this.isLoading = true;
-    this.authService.signup(input.value.email, input.value.password).subscribe(
+    if (!input.form.valid) return;
+    let authObs: Observable<Record<any, any>>;
+    if (this.isLoginMode)
+      authObs = this.authService.login(input.value.email, input.value.password);
+    else
+      authObs = this.authService.signup(
+        input.value.email,
+        input.value.password
+      );
+
+    authObs.subscribe(
       (resData) => {
         console.log(resData);
         this.isLoading = false;
       },
       (error) => {
         this.isLoading = false;
-        console.log(error);
+        this.error = error.message;
       }
     );
     input.reset();
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
   constructor(private readonly http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post("https://signup", { email, password });
+    return this.http.post("https://signup", { email, password }).pipe(
+      catchError((error) => {
+        return throwError(error);
+      })
+    );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post("https://loginurl", { email, password });
   }
 }
