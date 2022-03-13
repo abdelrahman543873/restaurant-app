c66506360d19cf251ff437cd9ad38cf687c95545
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2022-03-13 11:31:08 +0200

    feat: added auth logic

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,21 +1,32 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { throwError, Subject } from "rxjs";
+import { User } from "./user.model";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
   constructor(private readonly http: HttpClient) {}
 
+  user = new Subject<User>();
+
   signup(email: string, password: string) {
     return this.http.post("https://signup", { email, password }).pipe(
       catchError((error) => {
         return throwError(error);
-      })
+      }),
+      tap((resData: User) => this.handleAuthentication(resData))
     );
   }
 
   login(email: string, password: string) {
-    return this.http.post("https://loginurl", { email, password });
+    return this.http
+      .post("https://loginurl", { email, password })
+      .pipe(tap((resData: User) => this.handleAuthentication(resData)));
+  }
+
+  private handleAuthentication(input: User) {
+    const user = new User(input.email, "randomId", "randomString", new Date());
+    this.user.next(user);
   }
 }
